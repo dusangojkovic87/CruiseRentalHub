710b3f56f9dfdda9f1b6fd5927c37e0e9451f5f3
dusangojkovic87 <dusangojkovic087@gmail.com>
2024-01-08 18:24:47 +0100

    zapocet header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AccountComponent } from './pages/account/account.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { component: HomeComponent, path: 'home' },
+  { component: AccountComponent, path: 'account' },
+];
