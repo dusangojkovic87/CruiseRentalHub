5e9e167670a9c402b5578e76a44a4f4d361d02c8
dusangojkovic87 <dusangojkovic087@gmail.com>
2024-01-10 14:17:37 +0100

    responsive booking form dodato

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { HomeComponent } from './pages/home/home.component';
 import { AccountComponent } from './pages/account/account.component';
 
 export const routes: Routes = [
+  { component: HomeComponent, path: '' },
   { component: HomeComponent, path: 'home' },
   { component: AccountComponent, path: 'account' },
 ];
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../Shared/components/header/header.component';
 import { MainImagePanelComponent } from './components/main-image-panel/main-image-panel.component';
+import { BookingFormComponent } from '../../Shared/components/booking-form/booking-form.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, MainImagePanelComponent],
+  imports: [HeaderComponent, MainImagePanelComponent, BookingFormComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
